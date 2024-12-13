f8204de849f7d48ca07d3c18ab183777631bed22
HuyTX-LTS <huytx@ltsgroup.tech>
2024-12-13 17:36:05 +0700

    init template

---
+++ frontend/common/routes.ts
@@ -0,0 +1,18 @@
+export const APP_ROUTES = {
+  Dashboard: {
+    path: "/dashboard",
+    title: "Dashboard",
+  },
+  CV: {
+    path: "/cv",
+    title: "CVs",
+  },
+  JD: {
+    path: "/jd",
+    title: "JDs",
+  },
+  Table: {
+    path: "/table",
+    title: "Table",
+  },
+};
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,18 +1,28 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
+  darkMode: "class",
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      transitionProperty: {
+        width: "width",
+        spacing: "margin, padding",
+      },
+      animation: {
+        "spin-slow": "spin 4.5s linear infinite",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
